0668106fd2752df6501b73ab033ff6b92803e852
kaitobq <kaitobq520@gmail.com>
2024-02-01 17:23:34 +0900

    first commit

---
+++ src/components/BlogPage.js
@@ -0,0 +1,123 @@
+import React from "react";
+
+const BlogPage = () => {
+  return (
+    <div className="container">
+      <h1>ブログ</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea, ipsam
+        distinctio ex voluptatem natus nemo ut incidunt temporibus accusamus
+        eligendi facilis, error vero magnam culpa aut eveniet repellat
+        consequuntur hic odit nam possimus voluptatum. Repellendus cumque saepe
+        aspernatur, corrupti quae alias magnam, asperiores laborum assumenda
+        tempora obcaecati praesentium hic aut sint voluptas quod maiores. Ab
+        dolore, non, maiores nisi porro aperiam eligendi voluptatibus laborum
+        doloremque odio adipisci nostrum ullam necessitatibus laboriosam earum,
+        corporis quod ex repudiandae dolores. Fugit consectetur quam repellendus
+        aliquam exercitationem officiis natus? Officiis praesentium, ullam
+        fugiat vero totam cumque magnam, tenetur dignissimos, rerum odio nisi
+        repellat. Amet?
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea, ipsam
+        distinctio ex voluptatem natus nemo ut incidunt temporibus accusamus
+        eligendi facilis, error vero magnam culpa aut eveniet repellat
+        consequuntur hic odit nam possimus voluptatum. Repellendus cumque saepe
+        aspernatur, corrupti quae alias magnam, asperiores laborum assumenda
+        tempora obcaecati praesentium hic aut sint voluptas quod maiores. Ab
+        dolore, non, maiores nisi porro aperiam eligendi voluptatibus laborum
+        doloremque odio adipisci nostrum ullam necessitatibus laboriosam earum,
+        corporis quod ex repudiandae dolores. Fugit consectetur quam repellendus
+        aliquam exercitationem officiis natus? Officiis praesentium, ullam
+        fugiat vero totam cumque magnam, tenetur dignissimos, rerum odio nisi
+        repellat. Amet?
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea, ipsam
+        distinctio ex voluptatem natus nemo ut incidunt temporibus accusamus
+        eligendi facilis, error vero magnam culpa aut eveniet repellat
+        consequuntur hic odit nam possimus voluptatum. Repellendus cumque saepe
+        aspernatur, corrupti quae alias magnam, asperiores laborum assumenda
+        tempora obcaecati praesentium hic aut sint voluptas quod maiores. Ab
+        dolore, non, maiores nisi porro aperiam eligendi voluptatibus laborum
+        doloremque odio adipisci nostrum ullam necessitatibus laboriosam earum,
+        corporis quod ex repudiandae dolores. Fugit consectetur quam repellendus
+        aliquam exercitationem officiis natus? Officiis praesentium, ullam
+        fugiat vero totam cumque magnam, tenetur dignissimos, rerum odio nisi
+        repellat. Amet?
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea, ipsam
+        distinctio ex voluptatem natus nemo ut incidunt temporibus accusamus
+        eligendi facilis, error vero magnam culpa aut eveniet repellat
+        consequuntur hic odit nam possimus voluptatum. Repellendus cumque saepe
+        aspernatur, corrupti quae alias magnam, asperiores laborum assumenda
+        tempora obcaecati praesentium hic aut sint voluptas quod maiores. Ab
+        dolore, non, maiores nisi porro aperiam eligendi voluptatibus laborum
+        doloremque odio adipisci nostrum ullam necessitatibus laboriosam earum,
+        corporis quod ex repudiandae dolores. Fugit consectetur quam repellendus
+        aliquam exercitationem officiis natus? Officiis praesentium, ullam
+        fugiat vero totam cumque magnam, tenetur dignissimos, rerum odio nisi
+        repellat. Amet?
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea, ipsam
+        distinctio ex voluptatem natus nemo ut incidunt temporibus accusamus
+        eligendi facilis, error vero magnam culpa aut eveniet repellat
+        consequuntur hic odit nam possimus voluptatum. Repellendus cumque saepe
+        aspernatur, corrupti quae alias magnam, asperiores laborum assumenda
+        tempora obcaecati praesentium hic aut sint voluptas quod maiores. Ab
+        dolore, non, maiores nisi porro aperiam eligendi voluptatibus laborum
+        doloremque odio adipisci nostrum ullam necessitatibus laboriosam earum,
+        corporis quod ex repudiandae dolores. Fugit consectetur quam repellendus
+        aliquam exercitationem officiis natus? Officiis praesentium, ullam
+        fugiat vero totam cumque magnam, tenetur dignissimos, rerum odio nisi
+        repellat. Amet?
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea, ipsam
+        distinctio ex voluptatem natus nemo ut incidunt temporibus accusamus
+        eligendi facilis, error vero magnam culpa aut eveniet repellat
+        consequuntur hic odit nam possimus voluptatum. Repellendus cumque saepe
+        aspernatur, corrupti quae alias magnam, asperiores laborum assumenda
+        tempora obcaecati praesentium hic aut sint voluptas quod maiores. Ab
+        dolore, non, maiores nisi porro aperiam eligendi voluptatibus laborum
+        doloremque odio adipisci nostrum ullam necessitatibus laboriosam earum,
+        corporis quod ex repudiandae dolores. Fugit consectetur quam repellendus
+        aliquam exercitationem officiis natus? Officiis praesentium, ullam
+        fugiat vero totam cumque magnam, tenetur dignissimos, rerum odio nisi
+        repellat. Amet?
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea, ipsam
+        distinctio ex voluptatem natus nemo ut incidunt temporibus accusamus
+        eligendi facilis, error vero magnam culpa aut eveniet repellat
+        consequuntur hic odit nam possimus voluptatum. Repellendus cumque saepe
+        aspernatur, corrupti quae alias magnam, asperiores laborum assumenda
+        tempora obcaecati praesentium hic aut sint voluptas quod maiores. Ab
+        dolore, non, maiores nisi porro aperiam eligendi voluptatibus laborum
+        doloremque odio adipisci nostrum ullam necessitatibus laboriosam earum,
+        corporis quod ex repudiandae dolores. Fugit consectetur quam repellendus
+        aliquam exercitationem officiis natus? Officiis praesentium, ullam
+        fugiat vero totam cumque magnam, tenetur dignissimos, rerum odio nisi
+        repellat. Amet?
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea, ipsam
+        distinctio ex voluptatem natus nemo ut incidunt temporibus accusamus
+        eligendi facilis, error vero magnam culpa aut eveniet repellat
+        consequuntur hic odit nam possimus voluptatum. Repellendus cumque saepe
+        aspernatur, corrupti quae alias magnam, asperiores laborum assumenda
+        tempora obcaecati praesentium hic aut sint voluptas quod maiores. Ab
+        dolore, non, maiores nisi porro aperiam eligendi voluptatibus laborum
+        doloremque odio adipisci nostrum ullam necessitatibus laboriosam earum,
+        corporis quod ex repudiandae dolores. Fugit consectetur quam repellendus
+        aliquam exercitationem officiis natus? Officiis praesentium, ullam
+        fugiat vero totam cumque magnam, tenetur dignissimos, rerum odio nisi
+        repellat. Amet?
+      </p>
+    </div>
+  );
+};
+
+export default BlogPage;
